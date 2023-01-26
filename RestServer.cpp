42e09aafb036b67d278e308133a91bc9f495cfb4
AjJordy <jordyfaria0@gmail.com>
2023-01-26 10:12:53 -0300

    init

--- RestServer.cpp
+++ RestServer.cpp
@@ -1,26 +1,110 @@
 #include "RestServer.h"
 
-RestServer::RestServer(EthernetServer& server): server_(server), routesIndex_(0), bufferIndex_(0) {
+
+
+RestServer::RestServer(EthernetServer& server): server_(server), routesIndex_(0), bufferIndex_(0) {}
+
+String RestServer::getParam(char * text){
+	int begin = String(text).indexOf('=') + 1;
+	return String(text).substring(begin);
+}
+
+bool RestServer::connect(){
+	client_ = server_.available();
+	if(client_) return true;
+	else return false;
+}
+
+
+String RestServer::getBody(){
+  	String json = "";
+	long timeout = millis();
+  	while (millis() - timeout < 300) {
+		if (available()){
+			timeout = millis();
+			json += read();
+		}		
+	}
+ 
+	if (json.charAt(0) == '\'' && json.charAt(json.length()-1) == '\'') {
+		json = json.substring(1, json.length()-1);
+	}
+	return json;
+}
+
+
+int RestServer::getBody(char* buffer, int length){
+	int count = 0;
+	long timeout = millis();
+	while (count < length && millis() - timeout < 300) {
+		if (available()){
+			timeout = millis();
+			buffer[count] = read();
+			count++;
+		}
+	}
+	return count;
+}
+
+
+String RestServer::getBodyArray(){
+  String json = "";
+  long time = millis();
+  while(millis() - time < 1000) {
+    if (available()) {
+      time = millis();
+      char character = read();
+      json += character;
+      if (character == ']') break;
+    }
+  }
+  if (json.charAt(0) == '\'') {
+    json = json.substring(1);
+  }
+  return json;
+}
+
+void RestServer::stop(){
+  client_.stop();
+}
+
+char RestServer::read(){
+  return client_.read();
+}
+
+int RestServer::available(){
+  return client_.available();
 }
 
 void RestServer::run() {
-  client_ = server_.available();
-  if (client_) {
-    JSON_START();
-    // Check the received request and process it
-    check();
-    bufferIndex_--;
-    JSON_CLOSE();
+  if (server_.available()){
+     client_ = server_.available();
+    if (client_) {
+      JSON_START();
+      // Check the received request and process it
+      check();
+      bufferIndex_--;
+      JSON_CLOSE();
 
-    // Send data for the client
-    send(8, 0);
+      // Send data for the client
+      send(8, 0);
 
-    // Stop the client connection
-    client_.stop();
+      // Stop the client connection
+      client_.stop();
 
-    // Necessary resets
-    reset();
+      // Necessary resets
+      reset();
+    }
   }
+}
+
+void RestServer::loop() {
+  if (server_.available()) {
+    client_ = server_.available();
+    if (client_) {
+      check();
+    }
+  }  
 }
 
 void RestServer::reset() {
@@ -31,7 +115,7 @@ void RestServer::reset() {
   bufferIndex_ = 0;
 }
 
-void RestServer::addRoute(char * method, char * route, void (*f)(char * params) ) {
+void RestServer::addRoute(char * method, const char * route, void (*f)(char * params) ) {
   // memcpy(routes_[routesIndex_].name, route, strlen(route)+1);
   routes_[routesIndex_].method   = method;
   routes_[routesIndex_].name     = route;
@@ -49,8 +133,8 @@ void RestServer::addToBuffer(char * value) {
   bufferIndex_ = bufferIndex_ + strlen(value);
 }
 
-void RestServer::addData(char* name, char * value) {
-  char bufferAux[255] = {0};
+void RestServer::addData(const char* name, char * value) {
+  char bufferAux[5120] = {0};
   uint16_t idx = 0;
 
   // Format the data as:
@@ -73,17 +157,32 @@ void RestServer::addData(char* name, char * value) {
 }
 
 // Add to output buffer_
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(ESP8266) || defined(CORE_WILDFIRE)
-void RestServer::addData(char* name, String& value){
-  for (int i = 0; i < value.length(); i++){
-    buffer_[bufferIndex_+i] = value[i];  
+void RestServer::addData(const char* name, String& value){
+
+  char bufferAux[5120] = {0};
+  uint16_t idx = 0;
+
+  bufferAux[idx++] = '"';
+  for (int i = 0; i < strlen(name); i++){
+    bufferAux[idx++] = name[i];
   }
-  bufferIndex_ = bufferIndex_ + value.length();
+  bufferAux[idx++] = '"';
+
+  bufferAux[idx++] = ':';
+  bufferAux[idx++] = '"';
+  for (int i = 0; i < value.length(); i++){
+    bufferAux[idx++] = value.charAt(i);  
+  }
+  bufferAux[idx++] = '"';
+  bufferAux[idx++] = ',';
+
+  addToBuffer(bufferAux);
+
 }
-#endif
+
 
 // Add to output buffer_
-void RestServer::addData(char* name, uint16_t value){
+void RestServer::addData(const char* name, uint16_t value){
   char number[10];
   itoa(value,number,10);
   
@@ -91,7 +190,7 @@ void RestServer::addData(char* name, uint16_t value){
 }
 
 // Add to output buffer_
-void RestServer::addData(char* name, int value){
+void RestServer::addData(const char* name, int value){
   char number[10];
   itoa(value,number,10);
   
@@ -99,17 +198,18 @@ void RestServer::addData(char* name, int value){
 }
 
 // Add to output buffer_ (Mega & ESP only)
-// #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(ESP8266) || defined(CORE_WILDFIRE)
-void RestServer::addData(char* name, float value){
+void RestServer::addData(const char* name, float value){
   char number[10];
-  dtostrf(value, 5, 2, number);
-  
+  // dtostrf(value, 5, 2, number);  
+  char fmt[20];
+  sprintf(fmt, "%%%d.%df", 5, 2);
+  sprintf(number, fmt, value);  
+
   addData(name, number);
 }
-// #endif
 
 // Send the HTTP response for the client
-void RestServer::send(uint8_t chunkSize, uint8_t delayTime) {
+void RestServer::send(uint16_t chunkSize, uint16_t delayTime) {
   // First, send the HTTP Common Header
   client_.println(HTTP_COMMON_HEADER);
 
@@ -120,10 +220,10 @@ void RestServer::send(uint8_t chunkSize, uint8_t delayTime) {
   // Send chunk by chunk #####################################
 
   // Max iterations
-  uint8_t max = (int)(bufferIndex_/chunkSize) + 1;
+  uint16_t max = (int)(bufferIndex_/chunkSize) + 1;
 
   // Send data
-  for (uint8_t i = 0; i < max; i++) {
+  for (uint16_t i = 0; i < max; i++) {
     char bufferAux[chunkSize+1];
     memcpy(bufferAux, buffer_ + i*chunkSize, chunkSize);
     bufferAux[chunkSize] = '\0';
@@ -136,37 +236,48 @@ void RestServer::send(uint8_t chunkSize, uint8_t delayTime) {
   }
 }
 
+
+void RestServer::sendRaw(String mensagem){
+	// Serial.println("[sendRaw] "+mensagem);
+	for (int i=0; i<mensagem.length(); i+=1024){
+		client_.print(mensagem.substring(i, i+1024));
+	}	
+	// client_.flush();
+}
+
+void RestServer::sendBuffer(const char* mensagem, uint16_t lenght){
+	client_.write(mensagem, lenght);
+}
+
 // Extract information about the HTTP Header
 void RestServer::check() {
   char route[ROUTES_LENGHT] = {0};
   bool routePrepare = false;
   bool routeCatchFinished = false;
-  uint8_t r = 0;
+  uint16_t r = 0;
 
   char query[QUERY_LENGTH] = {0};
   bool queryPrepare = false;
   bool queryCatchFinished = false;
-  uint8_t q = 0;
+  uint16_t q = 0;
   
   char method[METHODS_LENGTH] = {0};
   bool methodCatchFinished = false;
-  uint8_t m = 0;
+  uint16_t m = 0;
 
   bool currentLineIsBlank = true;
   char c;
   while ( client_.connected() && client_.available() ) {
     c = client_.read();
+
     // DLOGChar(c);
 
-    // Start end of line process ////////////////
+    // Start end of line process /
     // if you've gotten to the end of the line (received a newline
     // character) and the line is blank, the http request header has ended,
     // so you can send a reply or check the body of the http header
     if (c == '\n' && currentLineIsBlank) {
       // Here is where the parameters of other HTTP Methods will be.
-      while(client_.available() && client_.connected())
-        query[q++] = (client_.read());
-      
       break;
     }
 
@@ -174,9 +285,9 @@ void RestServer::check() {
       currentLineIsBlank = true; // you're starting a new line
     else if (c != '\r')
       currentLineIsBlank = false; // you've gotten a character on the current line
-    // End end of line process //////////////////
+    // End end of line process ///
 
-    // Start route catch process ////////////////
+    // Start route catch process /
     if(c == '/' && !routePrepare)
       routePrepare = true;
 
@@ -185,9 +296,9 @@ void RestServer::check() {
 
     if(routePrepare && !routeCatchFinished)
       route[r++] = c;
-    // End route catch process //////////////////
+    // End route catch process 
 
-    // Start query catch process ////////////////
+    // Start query catch process 
     if(c == ' ' && queryPrepare)
       queryCatchFinished = true;
 
@@ -196,34 +307,34 @@ void RestServer::check() {
 
     if(c == '?' && !queryPrepare)
       queryPrepare = true;
-    // End query catch process /////////////////
+    // End query catch process 
 
-    // Start method catch process ///////////////
+    // Start method catch process 
     if(c == ' ' && !methodCatchFinished)
       methodCatchFinished = true;
 
     if(!methodCatchFinished)
       method[m++] = c;
-    // End method catch process /////////////////
+    // End method catch process 
 
   }
 
   for(int i = 0; i < routesIndex_; i++) {
       // Check if the routes names matches
-      if(strncmp( route, routes_[i].name, sizeof(routes_[i].name) ) != 0)
+      // if(strncmp( route, routes_[i].name, sizeof(routes_[i].name) ) != 0)
+      if (String(route) != String(routes_[i].name))
         continue;
 
       // Check if the HTTP METHOD matters for this route
-      if(strncmp( routes_[i].method, "*", sizeof(routes_[i].method) ) != 0) {
+      // if(strncmp( routes_[i].method, "*", sizeof(routes_[i].method) ) != 0) {
+      if (String(routes_[i].method) != "*") { 
         // If it matters, check if the methods matches
-        if(strncmp( method, routes_[i].method, sizeof(routes_[i].method) ) != 0)
+        // if(strncmp( method, routes_[i].method, sizeof(routes_[i].method) ) != 0)
+        if (String(method) != String(routes_[i].method))
           continue;
       }
 
       // Route callback (function)
-      // DLOG(route);
       routes_[i].callback(query);
-      LOG("Route callback!");
   }
-
 }
--- RestServer.h
+++ RestServer.h
@@ -2,14 +2,55 @@
 #define restserver_h
 
 // Include Arduino header
-#include "Arduino.h"
-#include "Log.h"
-#include <UIPEthernet.h>
-#include "RestSettings.h"
+#include <Arduino.h>
+#include <Ethernet.h>
+
+
+#ifndef ROUTES_TOTAL
+#define ROUTES_TOTAL 50
+#endif
+
+#ifndef ROUTES_LENGHT
+#define ROUTES_LENGHT 32
+#endif
+
+#ifndef QUERY_LENGTH
+#define QUERY_LENGTH 128
+#endif
+
+#ifndef HTTP_COMMON_HEADER
+#define HTTP_COMMON_HEADER "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: POST, GET, PUT, OPTIONS\r\nContent-Type: application/json\r\n\n"
+#endif
+
+#define HTTP_OK "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nConnection: close\r\n"
+#define HTTP_TEXT_TYPE "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: POST, GET, PUT, OPTIONS\r\nContent-Type: text\r\nConnection: close\r\n"
+#define HTTP_HTML "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: POST, GET, PUT, OPTIONS\r\nContent-Type: text/html\r\n\n"
+#define HTTP_ERROR "HTTP/1.1 400\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: POST, GET, PUT, OPTIONS\r\nContent-Type: text/html\r\nConnection: close\r\n"
+#define HTTP_ERROR_BODY "HTTP/1.1 400\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: application/json\r\n\n"
+
+
+#ifndef OUTPUT_BUFFER_SIZE
+#define OUTPUT_BUFFER_SIZE 1024
+#endif
+
+#define JSON_START() addToBuffer("{ ");
+#define JSON_CLOSE() addToBuffer(" }");
+
+
+#define METHODS_LENGTH 7
+#define ANY "*"
+#define GET "GET"
+#define PUT "PUT"
+#define HEAD "HEAD"
+#define POST "POST"
+#define TRACE "TRACE"
+#define DELETE "DELETE"
+#define OPTIONS "OPTIONS"
+
 
 struct Routes {
   char * method;
-  char * name;
+  const char * name;
   void (*callback)(char * params);
 };
 
@@ -18,28 +59,39 @@ public:
   RestServer(EthernetServer& client);
   
   void run();
+  void loop();
+  String getBody();
+  int getBody(char* buffer, int length);
+  String getBodyArray();
   
-  void addRoute(char * method, char * route, void (*f)(char *));
-  
-  void addData(char* name, String& value);
-  void addData(char* name, uint16_t value);
-  void addData(char* name, int value);
-  void addData(char* name, float value);
-  void addData(char* name, char* value);
+  void addRoute(char * method, const char * route, void (*f)(char *));
+  String getParam(char * text);  
+  void addData(const char* name, String& value);
+  void addData(const char* name, uint16_t value);
+  void addData(const char* name, int value);
+  void addData(const char* name, float value);
+  void addData(const char* name, char* value);
+  void sendRaw(String mensagem);
+  void sendBuffer(const char* mensagem, uint16_t index);
+  void send(uint16_t chunkSize, uint16_t delayTime);
+  void reset();
+  char read();
+  int available();
+  bool connect();
+  void stop();
+  EthernetClient client_;
 
 private:
   Routes routes_[ROUTES_TOTAL];
-  uint8_t routesIndex_;
+  uint16_t routesIndex_;
   char buffer_[OUTPUT_BUFFER_SIZE];
-  uint16_t bufferIndex_;
-  
+  uint16_t bufferIndex_;  
   EthernetServer& server_;
-  EthernetClient client_;
   
-  void check();
-  void reset();
+  void check();  
   void addToBuffer(char * value);
-  void send(uint8_t chunkSize, uint8_t delayTime);
+  
+  
 };
 
 #endif
